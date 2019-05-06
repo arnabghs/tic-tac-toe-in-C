7e3f3c3c665124272c2c75f56a28938ffa3e3f8a
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-05-07 00:18:32 +0530

    First player can choose his symbol and alternate symbols will be placed during the game.

--- game.c
+++ game.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Player
+{
+	char symbol;
+	int moves[5];
+};
+
 void displayBoard(char[]);
 int getPosition();
-void updateBoard(int, char[]);
-void turnManager(char[]);
+void updateBoard(int, char[], char);
+void turnManager(char[], struct Player *, struct Player *);
+void getPlayerSymbol(struct Player *, struct Player *);
 
 int main()
 {
 	char boardPositions[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
-	turnManager(boardPositions);
+	struct Player player1, player2;
+	struct Player *player1Pointer, *player2Pointer;
+	player1Pointer = &player1;
+	player2Pointer = &player2;
+
+	turnManager(boardPositions, player1Pointer, player2Pointer);
 
 	return 0;
 }
@@ -43,22 +55,30 @@ int getPosition()
 	return index;
 }
 
-void updateBoard(int index, char boardPositions[])
+void updateBoard(int index, char boardPositions[], char symbol)
 {
-	boardPositions[index] = 'X';
+	boardPositions[index] = symbol;
 }
 
-void turnManager(char boardPositions[])
+void turnManager(char boardPositions[], struct Player *player1Pointer, struct Player *player2Pointer)
 {
 	displayBoard(boardPositions);
-
+	getPlayerSymbol(player1Pointer, player2Pointer);
 	int turnPlayed = 0;
 
 	while (turnPlayed < 9)
 	{
+		char currentSymbol = (turnPlayed % 2 == 0) ? player1Pointer->symbol : player2Pointer->symbol;
 		int index = getPosition();
-		updateBoard(index, boardPositions);
+		updateBoard(index, boardPositions, currentSymbol);
 		displayBoard(boardPositions);
 		turnPlayed++;
 	}
+}
+
+void getPlayerSymbol(struct Player *player1Pointer, struct Player *player2Pointer)
+{
+	printf("player1 choose your symbol (X or O): \n");
+	scanf("%c", &player1Pointer->symbol);
+	player2Pointer->symbol = (player1Pointer->symbol == 'X') ? 'O' : 'X';
 }
